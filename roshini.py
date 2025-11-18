6fa812a4554ab1618376f0c5e8992e30d75f929a
ramyaaa0117-dev <ramyaaa.0117@gmail.com>
2025-11-18 18:09:17 +0530

    roshini.py

---
+++ roshini.py
@@ -0,0 +1,4 @@
+print("idhi python file kaabatti .py ivvaali ledhu java aithe roshini.java ani isthaav ok na")
+print("next nuvvu ila code rasesi akkada paina green commit undhiga idhi nokkuthaav akkada neeku default ga create roshini.py ani ostundhi nuv akkada create theeseyaali")
+print("Only roshini.py ane save cheyyaali ok naa")
+print("and akkada neeku main ani untundhi kada adhi alaane unchaali")
